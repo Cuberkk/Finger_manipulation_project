8beb05e7ccb54ccc9c946dfea55b8342662f1430
Kaiwen Zuo <kxz365@case.edu>
2026-02-09 14:14:33 -0500

    update the readme

--- Main_script.py
+++ Main_script.py
@@ -8,6 +8,6 @@ def main():
     print("Initialization complete. Starting to read data...")
     while True:
         biased_data = reader.read()
-        print(biased_data)
+        print(f"Thumb: Fx: {biased_data[0]}, Fy: {biased_data[1]}, Fz: {biased_data[2]}\n Index: Fx: {biased_data[6]}, Fy: {biased_data[7]}, Fz: {biased_data[8]},\n Ring: Fx: {biased_data[12]}, Fy: {biased_data[13]}, Fz: {biased_data[14]}\n")
         # Add any additional processing or saving of biased_data here
         # For example, you could save to a file, send over a network, etc.
--- README.md
+++ README.md
@@ -3,13 +3,13 @@ Sensor 1: FT44298
 Sensor 2: FT45281
 Sensor 3: FT44297
 
+NIDAQ Reading: (S1, S2)
+Labjack Reading: (S3)
+Main Reader Reading: Biased (S1, S2, S3)
+
 Gesture 1: 3 finger rotatory manipulation
 <ul>
     <li> Thumb for Sensor 1</li>
     <li> Index for Sensor 3</li>
     <li> Ring for Senosr 2</li>
 </ul>
-
-This is Kaiwen Test
-
-This is a test on local branch
